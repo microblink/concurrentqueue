0ae0896f5624ab17d08b263c847d451b8af29c74
Nenad Mikša <nenad.miksa@microblink.com>
2018-07-31 10:28:35 +0200

    fix header location in package

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class ConcurrentqueueConan(ConanFile):
     name = "ConcurrentQueue"
-    version = "1.0.0"
+    version = "1.0.1"
     license = "Boost Software License - Version 1.0"
     url = "https://github.com/microblink/concurrentqueue"
     description = "A fast multi-producer, multi-consumer lock-free concurrent queue for C++11"
@@ -16,9 +16,9 @@ class ConcurrentqueueConan(ConanFile):
     no_copy_source = True
 
     def package(self):
-        self.copy("internal/*.h", dst="include")
-        self.copy("concurrentqueue.h", dst="include")
-        self.copy("blockingconcurrentqueue.h", dst="include")
+        self.copy("internal/*.h", dst="include/concurrentqueue")
+        self.copy("concurrentqueue.h", dst="include/concurrentqueue")
+        self.copy("blockingconcurrentqueue.h", dst="include/concurrentqueue")
 
     def package_id(self):
         self.info.header_only()
